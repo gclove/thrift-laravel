e33184ffd4a12ccbb4b5f257832b72399d4ad690
gclove <gclove@live.com>
2018-05-22 11:16:06 +0000

    update for Laravel 5.6

--- src/Contracts/ThriftServer.php
+++ src/Contracts/ThriftServer.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Angejia\Thrift\Contracts;
+namespace Gclove\Thrift\Contracts;
 
 use Thrift\Transport\TTransport;
 
--- src/Middleware/ThriftServerMiddleware.php
+++ src/Middleware/ThriftServerMiddleware.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Angejia\Thrift\Middleware;
+namespace Gclove\Thrift\Middleware;
 
-use Angejia\Thrift\Contracts\ThriftServer;
+use Gclove\Thrift\Contracts\ThriftServer;
 use Closure;
 use Illuminate\Http\Response;
 use Thrift\Transport\TMemoryBuffer;
